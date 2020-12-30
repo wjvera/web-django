ab3d885a53d3bc72ea06883c1517f0996b539fcd
wjvera <washington666xxx@gmail.com>
2020-12-30 11:57:29 -0500

    subiendo

---
+++ webpersonal/core/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, HttpResponse
+
+html_base= """
+        <h1>Mi web personal</h1>
+        <ul>
+            <li>
+                <a href="/">Portada</a>
+            </li>
+            <li>
+                <a href="/about/">Acerca de...</a>
+            </li>
+            <li>
+                <a href="/portfolio/">Portafolio</a>
+            </li>
+            <li>
+                <a href="/contact/">Contacto del grupo...</a>
+            </li>
+        </ul>
+"""
+
+# Create your views here.
+
+def home(request):
+    return render(request, "core/home.html")
+
+def about(request):
+    return render(request, "core/about.html")
+
+def contact(request):
+    return render(request, "core/contact.html")
